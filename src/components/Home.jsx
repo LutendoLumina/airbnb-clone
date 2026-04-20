4a647ccd6872688148b8a3f6b029eabe1c17d75f
Lutendo Matshidze <Lupreshie@gmail.com>
2026-04-20 14:53:27 +0200

    Added the card component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,47 @@
-import Banner from './Banner'
+import Banner from "./Banner";
+import Card from "./Card";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div className="home">
       <Banner />
+      <div className="home_section">
+        <Card
+          src="https://a0.muscache.com/im/pictures/hosting/Hosting-1612961478478331433/original/194e8a13-bff2-40b3-83c4-3778b88670d7.jpeg?im_w=1200"
+          title="Online Experience"
+          description="Unique activities we can do together, led by a world of hosts"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/hosting/Hosting-1612961478478331433/original/194e8a13-bff2-40b3-83c4-3778b88670d7.jpeg?im_w=1200"
+          title="Online Experience"
+          description="Unique activities we can do together, led by a world of hosts"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/hosting/Hosting-1612961478478331433/original/194e8a13-bff2-40b3-83c4-3778b88670d7.jpeg?im_w=1200"
+          title="Online Experience"
+          description="Unique activities we can do together, led by a world of hosts"
+        />
+      </div>
+      <div className="home_section">
+        <Card
+          src="https://a0.muscache.com/im/pictures/hosting/Hosting-1612961478478331433/original/194e8a13-bff2-40b3-83c4-3778b88670d7.jpeg?im_w=1200"
+          title="Online Experience"
+          description="Unique activities we can do together, led by a world of hosts"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/hosting/Hosting-1612961478478331433/original/194e8a13-bff2-40b3-83c4-3778b88670d7.jpeg?im_w=1200"
+          title="Online Experience"
+          description="Unique activities we can do together, led by a world of hosts"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/hosting/Hosting-1612961478478331433/original/194e8a13-bff2-40b3-83c4-3778b88670d7.jpeg?im_w=1200"
+          title="Online Experience"
+          description="Unique activities we can do together, led by a world of hosts"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
